62a180f9714567c58e7072e20eb78984964469c5
xiaohuting <826473959@qq.com>
2019-10-13 20:09:02 +0800

    change

--- magic.py
+++ magic.py
@@ -3,3 +3,4 @@ for magic in magics:
   print(magic.title()+",that was a great trick")
   print ("I can't wait to see your next trick.\n")
   print("i know")
+print ("I can't wait to see your next trick.\n")
